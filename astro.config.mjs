d27e9cf95f123e54c2bc1faa9bb97c93cd0546e8
sumit7577 <asmitkumarr082@gmail.com>
2023-02-15 20:30:37 +0530

    changes to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 // https://astro.build/config
 
 export default defineConfig({
