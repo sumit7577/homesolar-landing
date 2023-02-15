5aaa7543c0d12f7ce1b70b81a7da996f752c4bde
sumit7577 <asmitkumarr082@gmail.com>
2023-02-15 20:25:20 +0530

    added analytic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,23 @@
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import vercel from '@astrojs/vercel/serverless';
+// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  site: "https://www.homesolar.co.in",
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  })],
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
+      external: ["svgo"]
+    }
   },
+  output: "server",
+  adapter: vercel({
+    analytics: true
+  })
 });
