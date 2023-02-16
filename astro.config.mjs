30e450fe5284a5272df83254ca00f38be4c9935a
sumit7577 <asmitkumarr082@gmail.com>
2023-02-16 22:46:58 +0530

    crated privacy policy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@ import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import vercel from '@astrojs/vercel/static';
+import AstroPWA from '@vite-pwa/astro'
+
 // https://astro.build/config
 
 export default defineConfig({
   site: "https://www.homesolar.co.in",
   integrations: [tailwind(), image({
     serviceEntryPoint: "@astrojs/image/sharp"
-  })],
+  }), AstroPWA({ injectRegister: "auto" })],
   vite: {
     ssr: {
       external: ["svgo"]
